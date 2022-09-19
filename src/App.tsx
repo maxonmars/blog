f9c19ec3782ce3c9da31124ddd4670e8338b4f95
maxonmars <hydrocrub@gmail.com>
2022-09-19 22:19:45 +0700

    add React.lazy create async chunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import Counter from "./components/Counter";
 import './index.scss'
+import {Suspense} from "react";
+import {NavLink, Route, Routes} from "react-router-dom";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
+import React from "react";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 export const App = () => {
+    const activeStyle = ({isActive}: { isActive: boolean }): React.CSSProperties => isActive ? {color: 'red'} : {color: 'green'}
     return (
         <div className="app">
-            <Counter/>
+            <NavLink style={activeStyle} to='/about'>О сайте</NavLink>
+            <NavLink style={activeStyle} to='/'>Главная страница</NavLink>
+            <Suspense fallback={<div>...Loading</div>}>
+                <Routes>
+                    <Route path='/about' element={<AboutPageAsync/>}/>
+                    <Route path='/' element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
