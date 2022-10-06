6de65277fc838531a4fa87b93af76eaf72575078
maxonmars <hydrocrub@gmail.com>
2022-10-06 21:34:07 +0700

    Testing: classNames util

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,7 +1,26 @@
 import {classNames} from './classNames';
 
 describe('classNames', () => {
-	test('test', () => {
+	test('single class', () => {
 		expect(classNames(['someClass'])).toBe('someClass');
+	});
+
+	test('multiClass', () => {
+		expect(classNames(['someClass', 'someClass2'])).toBe('someClass someClass2');
+	});
+
+	test('filtering undefined', () => {
+		expect(classNames([undefined])).toBe('');
+	});
+
+	test('empty arguments', () => {
+		expect(classNames()).toBe('');
+	});
+
+	test('with mods', () => {
+		expect(classNames(
+			['someClass'],
+			{isValue: true, isValue2: false}))
+			.toBe('someClass isValue');
 	});
 });
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,6 +1,6 @@
 type Mods = Record<string, boolean>;
 
-export const classNames = (classes: string[], mods: Mods = {}): string => [
+export const classNames = (classes: string[] = [], mods: Mods = {}): string => [
 	...classes,
 	...Object.entries(mods)
 		.filter(([_, value]) => value)
